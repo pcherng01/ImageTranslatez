3c97638625a635b8943c51b0073e713b44581462
kuminin <kumin.in@hotmail.com>
2015-11-22 00:18:26 -0800

    change

--- cloud/main.js
+++ cloud/main.js
@@ -54,7 +54,7 @@ Parse.Cloud.define("getPhoto", function(request, response) {
             //response.success("Successfully retrieved " + results.length);
             var object = results[0];
             img = object.get("image").url();
-            $('picture')[0].src = img;
+            document.getElementById('picture').src = img;
             response.success("Successfully retrieved " + results.length + ".\n" + img);
 
         },
