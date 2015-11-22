10857a89f35deb26d7c640034e37ea7db293c27b
kuminin <kumin.in@hotmail.com>
2015-11-22 00:38:47 -0800

    change

--- cloud/main.js
+++ cloud/main.js
@@ -11,30 +11,30 @@ var object;
 // This returns a description of the photo in a json format.
 
 Parse.Cloud.define("getPhotoData", function(request, response) {
-    Parse.Cloud.httpRequest({
-        url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
-        params: {
-            url: img,
-            apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
-            outputMode: "json"
-        }
-    }).then(function(httpResponse) {
-        response.success(httpResponse.text);
-        var photo = Parse.Object.extend("PhotoObject");
-        var query = new Parse.Query(photo);
+    // Parse.Cloud.httpRequest({
+    //     url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
+    //     params: {
+    //         url: img,
+    //         apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
+    //         outputMode: "json"
+    //     }
+    // }).then(function(httpResponse) {
+    //     response.success(httpResponse.text);
+    //     var photo = Parse.Object.extend("PhotoObject");
+    //     var query = new Parse.Query(photo);
 
-        query.equalTo("ImageKey", "ImageFile");
-        query.find({
-            success: function(result) {
-                result.destroy(results[0]);
-            },
-            error: function(error) {
-                console.log("Error in delete Query")
-            }
-        });
-    }, function(httpResponse) {
-        console.error('Request failed');
-    });
+    //     query.equalTo("ImageKey", "ImageFile");
+    //     query.find({
+    //         success: function(result) {
+    //             result.destroy(results[0]);
+    //         },
+    //         error: function(error) {
+    //             console.log("Error in delete Query")
+    //         }
+    //     });
+    // }, function(httpResponse) {
+    //     console.error('Request failed');
+    // });
 });
 
 // Defines a method getPhoto to get a photo that is saved in to the
@@ -52,7 +52,30 @@ Parse.Cloud.define("getPhoto", function(request, response) {
             img = object.get("image").url();
             //document.getElementById('picture').src = img;
             response.success("Successfully retrieved " + results.length + ".\n" + img);
+            Parse.Cloud.httpRequest({
+                url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
+                params: {
+                    url: img,
+                    apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
+                    outputMode: "json"
+                }
+            }).then(function(httpResponse) {
+                response.success(httpResponse.text);
+                var photo = Parse.Object.extend("PhotoObject");
+                var query = new Parse.Query(photo);
 
+                query.equalTo("ImageKey", "ImageFile");
+                query.find({
+                    success: function(result) {
+                        result.destroy(results[0]);
+                    },
+                    error: function(error) {
+                        console.log("Error in delete Query")
+                    }
+                });
+            }, function(httpResponse) {
+                console.error('Request failed');
+            });
         },
         error: function(error) {
             console.error("Query Unsuccessful");
