0913c989cc6b652b00dc84f25bb539fd16cfbbc7
kuminin <kumin.in@hotmail.com>
2015-11-21 22:46:30 -0800

    fix call back error

--- cloud/main.js
+++ cloud/main.js
@@ -11,37 +11,37 @@ var image = "";
 
 Parse.Cloud.define("getPhotoData", function(request, response) {
     Parse.Cloud.run("getPhoto", {
-      success: function(results) {
-        console.log("Success!");
-      },
-      error: function(error) {
-        console.log("Fail");
-      }
-    });
-    Parse.Cloud.httpRequest({
-        url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
-        params: {
-            url: image,
-            apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
-            outputMode: "json",
-            imagePostMode: "not-raw"
-        }
-    }).then(function(httpResponse) {
-        response.success(httpResponse.text);
-        var photo = Parse.Object.extend("PhotoObject");
-        var query = new Parse.Query(photo);
+        success: function(results) {
+            console.log("Success!");
+            Parse.Cloud.httpRequest({
+                url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
+                params: {
+                    url: image,
+                    apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
+                    outputMode: "json",
+                    imagePostMode: "not-raw"
+                }
+            }).then(function(httpResponse) {
+                response.success(httpResponse.text);
+                var photo = Parse.Object.extend("PhotoObject");
+                var query = new Parse.Query(photo);
 
-        query.equalTo("image", "Image.jpg");
-        query.find({
-          success: function(result) {
-            result.destroy(results[0]);
-          },
-          error: function(error) {
-            console.log("Error in delete Query")
-          }
-        });
-    }, function(httpResponse) {
-        console.error('Request failed');
+                query.equalTo("image", "Image.jpg");
+                query.find({
+                    success: function(result) {
+                        result.destroy(results[0]);
+                    },
+                    error: function(error) {
+                        console.log("Error in delete Query")
+                    }
+                });
+            }, function(httpResponse) {
+                console.error('Request failed');
+            });
+        },
+        error: function(error) {
+            console.log("Fail");
+        }
     });
 });
 
@@ -49,18 +49,18 @@ Parse.Cloud.define("getPhotoData", function(request, response) {
 // parse database.
 
 Parse.Cloud.define("getPhoto", function(request, request) {
-  var photo = Parse.Object.extend("PhotoObject");
-  var query = new Parse.Query(photo);
+    var photo = Parse.Object.extend("PhotoObject");
+    var query = new Parse.Query(photo);
 
-  query.equalTo("image", "Image.jpg");
-  query.find({
-    success: function(results) {
-      console.log("Successfully retrieved " + results.length);
-      var object = results[0];
-      image = object.get("image");
-    },
-    error: function(error) {
-      console.error("Query Unsuccessful");
-    }
-  });
+    query.equalTo("image", "Image.jpg");
+    query.find({
+        success: function(results) {
+            console.log("Successfully retrieved " + results.length);
+            var object = results[0];
+            image = object.get("image");
+        },
+        error: function(error) {
+            console.error("Query Unsuccessful");
+        }
+    });
 });
