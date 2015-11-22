62c3af4cd4f208ebc55fcfd27e88c32810fb71c6
kuminin <kumin.in@hotmail.com>
2015-11-21 22:32:34 -0800

    Added getPhoto method and updated getPhotoData to be able to erase the found image after the json data has been returned

--- cloud/main.js
+++ cloud/main.js
@@ -1,21 +1,58 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    response.success("Hello world!");
 });
 
+var image = "";
 
-Parse.Cloud.define("getData", function(request, response) {
-  Parse.Cloud.httpRequest({
-    url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
-    params: {
-      url: "http://animalia-life.com/image.php?pic=/data_images/dog/dog1.jpg",
-      apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
-      outputMode: "json"
+// Defines a method getPhotoData to use the IBM alchemy API.
+// This returns a description of the photo in a json format.
+
+Parse.Cloud.define("getPhotoData", function(request, response) {
+    Parse.Cloud.httpRequest({
+        url: "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords",
+        params: {
+            url: image,
+            apikey: "c31bfed3d391144d692d4290a39d677bb73f10b8",
+            outputMode: "json",
+            imagePostMode: "not-raw"
+        }
+    }).then(function(httpResponse) {
+        response.success(httpResponse.text);
+        var photo = Parse.Object.extend("PhotoObject");
+        var query = new Parse.Query(photo);
+
+        query.equalTo("image", "Image.jpg");
+        query.find({
+          success: function(result) {
+            result.destroy(results[0]);
+          },
+          error: function(error) {
+            console.log("Error in delete Query")
+          }
+        });
+    }, function(httpResponse) {
+        console.error('Request failed');
+    });
+});
+
+// Defines a method getPhoto to get a photo that is saved in to the
+// parse database.
+
+Prase.Cloud.define("getPhoto", function(request, request) {
+  var photo = Parse.Object.extend("PhotoObject");
+  var query = new Parse.Query(photo);
+
+  query.equalTo("image", "Image.jpg");
+  query.find({
+    success: function(results) {
+      console.log("Successfully retrieved " + results.length);
+      var object = results[0];
+      image = object.get("image");
+    },
+    error: function(error) {
+      console.error("Query Unsuccessful");
     }
-  }).then(function(httpResponse) {
-      response.success(httpResponse.text);
-  },function(httpResponse) {
-    console.error('Request failed');
   });
 });
