268d60d0d1e26672a7d4e96813597547a8d494b4
pcherng01 <pcherng01@gmail.com>
2015-11-21 23:34:14 -0800

    Nothing

--- cloud/main.js
+++ cloud/main.js
@@ -50,7 +50,7 @@ Parse.Cloud.define("getPhoto", function(request, response) {
             response.success("Successfully retrieved " + results.length);
             var object = results[0];
             img = object.get("image").url();
-            response.success(image);
+            response.success(img);
         },
         error: function(error) {
             console.error("Query Unsuccessful");
